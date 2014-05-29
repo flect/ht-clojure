ad1b4dffa2121b392a46c92dd1c972280a201148
o-seirou <o-seirou@flect.co.jp>
2014-05-29 09:14:05 +0000

    first commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject helloworld "0.0.1"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring/ring-jetty-adapter "1.2.2"]]
+  :uberjar-name "helloworld-standalone.jar"
+  :min-lein-version "2.0.0")
+
